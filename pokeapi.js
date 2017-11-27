ab1e2a538de3b434e3be69a425adc5344a1614f1
Marina <dubovik.m14@gmail.com>
2017-11-27 23:33:11 +0200

    add task01-2

--- pokeapi.js
+++ pokeapi.js
@@ -4,8 +4,27 @@ const axios = require('axios');
 // task01 - 1
 console.log('======= task01 - 1 =======')
 axios.get('https://pokeapi.co/api/v2/pokemon/42/')
-    .then(response => {
+    .then((response) => {
         console.log(`name: ${response.data.name},\nweight: ${response.data.weight},\nheight: ${response.data.height}`)
     })
 
 // task01 - 2 
+console.log('======= task01 - 2 =======')
+let requests = [];
+for(let i = 0; i < 3; i++) {
+    requests.push(axios.get('http://pokeapi.co/api/v2/pokemon/?limit=10'));
+}
+Promise.all(requests)
+    .then((results) => {
+        results.forEach((value, index) => {
+            value.data.results.forEach((pokemon, pokeInd) => {
+                console.log(`${index}${pokeInd} - ${pokemon.name}`)
+            })
+            console.log('---------------\n');
+        })
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+
+// task01 - 3
