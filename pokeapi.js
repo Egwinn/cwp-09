a3c9b23665c4ab0719acf770acbdd9465b494763
Marina <dubovik.m14@gmail.com>
2017-11-27 23:58:07 +0200

    add task01-3

--- pokeapi.js
+++ pokeapi.js
@@ -2,20 +2,20 @@ const Promise = require('bluebird');
 const axios = require('axios');
 
 // task01 - 1
-console.log('======= task01 - 1 =======')
 axios.get('https://pokeapi.co/api/v2/pokemon/42/')
     .then((response) => {
+        console.log('======= task01 - 1 =======');
         console.log(`name: ${response.data.name},\nweight: ${response.data.weight},\nheight: ${response.data.height}`)
     })
 
 // task01 - 2 
-console.log('======= task01 - 2 =======')
 let requests = [];
 for(let i = 0; i < 3; i++) {
     requests.push(axios.get('http://pokeapi.co/api/v2/pokemon/?limit=10'));
 }
 Promise.all(requests)
     .then((results) => {
+        console.log('======= task01 - 2 =======');
         results.forEach((value, index) => {
             value.data.results.forEach((pokemon, pokeInd) => {
                 console.log(`${index}${pokeInd} - ${pokemon.name}`)
@@ -28,3 +28,13 @@ Promise.all(requests)
     })
 
 // task01 - 3
+Promise.any([
+    axios.get('https://pokeapi.co/api/v2/pokemon/1/'),
+    axios.get('https://pokeapi.co/api/v2/pokemon/4/'),
+    axios.get('https://pokeapi.co/api/v2/pokemon/7/')    
+]).then((value) => {
+    console.log('======= task01 - 3 =======');
+    console.log(value.data.name);
+}).catch((error) => {
+    console.error(error);
+})
