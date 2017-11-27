77b6c71f4c460363e36d1ba7a2b1f233812e08ba
Marina <dubovik.m14@gmail.com>
2017-11-27 23:06:57 +0200

    add task01-1

--- pokeapi.js
+++ pokeapi.js
@@ -1,2 +1,11 @@
 const Promise = require('bluebird');
 const axios = require('axios');
+
+// task01 - 1
+console.log('======= task01 - 1 =======')
+axios.get('https://pokeapi.co/api/v2/pokemon/42/')
+    .then(response => {
+        console.log(`name: ${response.data.name},\nweight: ${response.data.weight},\nheight: ${response.data.height}`)
+    })
+
+// task01 - 2 
